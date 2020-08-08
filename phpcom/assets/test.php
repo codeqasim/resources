7f2cd9c78e34b4915a6a10b528862d87a47dfb55
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2020-08-09 04:57:59 +0500

    php com

---
+++ phpcom/assets/test.php
@@ -0,0 +1,8 @@
+<?php         header("Content-Type: text/xml;charset=iso-8859-1");
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>ABCD</loc>
+        <priority>1.0</priority>
+    </url>
+</urlset>
